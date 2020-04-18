dd1e4e802bbd57957259a763a931e105507503f1
bbedevian <brettbedevian@gmail.com>
2020-04-18 14:52:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+
+
+function scuberGreetingForFeet(someValue){
+  let cost;
   // Write your code here!
+  if (someValue <= 400) {
+    cost = "This one is on me!";
+  }
+  else if (someValue > 2500) {
+    cost = "No can do.";
+  }
+  else if (someValue > 2000) {
+    cost = 'I will gladly take your thirty bucks.';
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response;
+  if (city === "NYC") {
+  response = "Ok, sounds good.";
+  }
+  else {
+  response = "No go.";
+  }
+ return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ let response;
+ switch (tip) {
+   case 'generous':
+     response = "Thank you so much.";
+     break;
+   case 'not as generous':
+     response = "Thank you.";
+     break;
+   default:
+     response = "Bye.";
+     break;
+ }
+ return response;
+  
 }
